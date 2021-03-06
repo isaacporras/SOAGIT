2fc49398f667a0bfbcf131982874066ee874f7ac
kennethGHS <khken21@gmail.com>
2021-03-05 19:31:00 -0600

    Updates java

--- java-rest-api/src/com/company/Main.java
+++ java-rest-api/src/com/company/Main.java
@@ -23,5 +23,33 @@ public class Main {
             i--;
             i++;
         }
+        for (int i = 0; i < 1; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i <2 ; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i < 3; i++) {
+            i--;
+            i++;
+        }
+        for (int i = 0; i < 1; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i <2 ; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i < 3; i++) {
+            i--;
+            i++;
+        }
     }
 }
