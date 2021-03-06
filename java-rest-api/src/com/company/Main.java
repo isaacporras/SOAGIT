c6f8e0ee1d7e51e54f205afe0c21e2b972189d2c
Isaac Porras <isaacpope@estudiantec.cr>
2021-03-06 01:35:51 -0600

    Mis cambios

--- java-rest-api/src/com/company/Main.java
+++ java-rest-api/src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Este es un taller");
-        for (int i = 0; i < ; i++) {
-            i--;
-            i++;
-        }
+	System.out.println("Se quiere generar un conflicto");
+
     }
 }
