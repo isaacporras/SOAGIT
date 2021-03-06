3a5ca005780482e4d8c3eb72522afb70119f6770
kennethGHS <khken21@gmail.com>
2021-03-05 19:19:30 -0600

    Modifies file

--- java-rest-api/src/com/company/Main.java
+++ java-rest-api/src/com/company/Main.java
@@ -4,7 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Este es un taller");
-        for (int i = 0; i < ; i++) {
+        for (int i = 0; i < 23; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i < 1; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i <2 ; i++) {
+            i--;
+            i++;
+        }
+        System.out.println("Este es un taller");
+        for (int i = 0; i < 3; i++) {
             i--;
             i++;
         }
